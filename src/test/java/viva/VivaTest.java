77e0242cc41e1337b00bd918b470c0278f5fe019
reravindran <reravindran@deloitte.com.au>
2019-07-10 15:24:06 +1000

    Report location change

--- src/test/java/viva/VivaTest.java
+++ src/test/java/viva/VivaTest.java
@@ -1,11 +1,40 @@
 package viva;
 
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.BeforeClass;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-@RunWith(Karate.class)
+//@RunWith(Karate.class)
+@KarateOptions(tags = {"~@ignore"})
 public class VivaTest {
     // this will run all *.feature files that exist in sub-directories
     // refer to https://github.com/intuit/karate#naming-conventions
     // for folder-structure recommendations and naming conventions
+
+    @Test
+    public void testParallel() {
+        System.setProperty("karate.env", "demo"); // ensure reset if other tests (e.g. mock) had set env in CI
+        Results results = Runner.parallel(getClass(), 5);
+        generateReport(results.getReportDir());
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "VivaAPI");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
